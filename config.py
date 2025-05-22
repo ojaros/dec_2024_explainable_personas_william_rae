8a8c3d70e3cea33b7bf2c23677f347735a7ea8ee
Oliver Jaros <oliverjaros214@gmail.com>
2025-05-22 13:40:32 -0500

    Refactor config to dataclass

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
-max_subclusters = 25
-max_clusters = 10
-min_subcluster_size = 25
+from dataclasses import dataclass
 
-print(max_subclusters)
+@dataclass
+class AnalysisConfig:
+    """Configuration options for clustering analysis."""
+    min_clusters: int = 2
+    max_clusters: int = 10
+    min_subclusters: int = 2
+    max_subclusters: int = 25
+    encoded: bool = True
+    num_epochs: int = 50
+    min_cluster_size: int = 25
